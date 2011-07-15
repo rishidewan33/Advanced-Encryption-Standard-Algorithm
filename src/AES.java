51903307912d004c465e68d3b58d1de798c41b43
Rishi <Rishi@Rishi-PC>
2011-07-14 19:14:00 -0500

    Recommit

--- src/AES.java
+++ src/AES.java
@@ -11,6 +11,8 @@ public class AES {
      * S-BOX table used for Key Expansion and Sub-Bytes.
      */
 
+    public static enum Mode { ECB,CBC };
+    
     public static final int[][] sbox = {{0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76}, {0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0}, {0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15}, {0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75}, {0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84}, {0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf}, {0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8}, {0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2}, {0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73}, {0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb}, {0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79}, {0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08}, {0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a}, {0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e}, {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf}, {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16}};
 
     /**
@@ -53,9 +55,18 @@ public class AES {
         0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63,
         0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd,
         0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb};
-
+    
+    static String key = "";
+    static String iv = "";
+    static String ftw = "";
+    static BufferedReader keyreader;
+    static BufferedReader input;
+    static Mode mode;
+    static FileWriter out;
+    static int keyFileIndex = 1;
+    
     /**
-     * AES constructor for which there's  nothing to do.
+     * Empty AES constructor.
      */
     public AES() {
         //Nothing to initialize here.
@@ -66,7 +77,8 @@ public class AES {
      * Usage: java AES e|d [-length] [-mode] keyFile inputFile
      * @param args Array of command line arguments.
      */
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException 
+    {
         /*
          * args[0] should be either "e" or "d"
          * args[1] and args[2] should correspond to the following:
@@ -77,114 +89,155 @@ public class AES {
          *
          * args[3] and args[4] should exist only if -length was specified:
          */
-        try {
-            String key = "";
-            String ftw = "";
-            BufferedReader input;
-            FileWriter out;
+        try 
+        {
+            int keysizecheck = 128; //User's intended key size.
             if (!args[1].equals("-length")) //defaults to 128-bit
             {
-                key = (new BufferedReader(new FileReader(args[1]))).readLine();
-                if (key.length() * 4 != 128) {
-                    throw new Exception("Error: Attemping to use a " + key.length() * 4 + "-bit key with AES-128"); //Unmatched key argument and key input
+                if(!args[1].equals("-mode")) //No optional arguments given;
+                {
+                    
                 }
-                input = new BufferedReader(new FileReader(args[2]));
-                ftw += args[2];
-            } else {
-                key = (new BufferedReader(new FileReader(args[3]))).readLine();
-                if (key.length() * 4 != Integer.parseInt(args[2])) {
-                    throw new Exception("Error: Attemping to use a " + key.length() * 4 + "-bit key with AES-" + args[2]);
+                else //Mode option was given;
+                {
+                    mode = args[2].equals("ecb") ? Mode.ECB : Mode.CBC;
+                    keyFileIndex += 2;
                 }
-                input = new BufferedReader(new FileReader(args[4]));
-                ftw += args[4]; //ftw => File To Write
-
+            } 
+            else //-length was explicitly given.
+            {
+                keyFileIndex+=2;
+                keysizecheck = Integer.parseInt(args[keyFileIndex-1]);
+                if(args[4].equals("-mode")) //Both -length and -mode options were given
+                {
+                    mode = args[5].equals("ecb") ? Mode.ECB : Mode.CBC;
+                    keyFileIndex+=2;
+                }
+                
             }
-
-            AES aes = new AES();
-            if (args[0].equalsIgnoreCase("e")) {
-                double time1 = (double) System.currentTimeMillis();
-                out = new FileWriter(ftw + ".enc");
-                int numRounds = 10 + (((key.length() * 4 - 128) / 32));
-                String line = input.readLine();
-                int[][] bytematrix;
-                int[][] keymatrix = aes.keySchedule(key);
-                while (line != null) {
-                    if (aes.validline(line)) {
-                        if (line.length() < 32) {
-                            line = String.format("%" + 32 + "s", line).replace(' ', '0');
-                        }
-                        bytematrix = new int[4][4];
-                        for (int i = 0; i < 4; i++) //Parses line into a matrix
-                        {
-                            for (int j = 0; j < 4; j++) {
-                                bytematrix[j][i] = Integer.parseInt(line.substring((8 * i) + (2 * j), (8 * i) + (2 * j + 2)), 16);
-                            }
-                        }
-                        aes.addRoundKey(bytematrix, aes.subKey(keymatrix, 0)); //Starts the addRoundKey with the first part of Key Expansion
-                        for (int i = 1; i < numRounds; i++) {
-                            aes.subBytes(bytematrix); //implements the Sub-Bytes subroutine.
-                            aes.shiftRows(bytematrix); //implements Shift-Rows subroutine.
-                            aes.mixColumns(bytematrix);
-                            aes.addRoundKey(bytematrix, aes.subKey(keymatrix, i));
-                        }
-                        aes.subBytes(bytematrix); //implements the Sub-Bytes subroutine.
-                        aes.shiftRows(bytematrix); //implements Shift-Rows subroutine.
-                        aes.addRoundKey(bytematrix, aes.subKey(keymatrix, numRounds));
-                        out.write(MatrixToString(bytematrix) + '\n');
-                        line = input.readLine();
-                    } else {
-                        line = input.readLine();
-                    }
-                }
-                double time2 = (double) System.currentTimeMillis();
-                input.close();
-                out.close();
-                File f = new File(ftw + ".enc");
-                double total = ((time2 - time1) / 1000.0);
-                System.out.println("Encryption throughput: " + ((double) (f.length() / 1000000.0) / total) + " MB/s");
-            } else if (args[0].equalsIgnoreCase("d")) {
-                double time1 = (double) System.currentTimeMillis();
-                out = new FileWriter(ftw + ".dec");
-                int numRounds = 10 + (((key.length() * 4 - 128) / 32));
-                String line = input.readLine();
-                int[][] bytematrix;
-                int[][] keymatrix = aes.keySchedule(key);
-
-                while (line != null) {
-                    bytematrix = new int[4][4];
-                    for (int i = 0; i < bytematrix.length; i++) //Parses line into a matrix
-                    {
-                        for (int j = 0; j < bytematrix[0].length; j++) {
-                            bytematrix[j][i] = Integer.parseInt(line.substring((8 * i) + (2 * j), (8 * i) + (2 * j + 2)), 16);
-                        }
-                    }
-                    aes.addRoundKey(bytematrix, aes.subKey(keymatrix, numRounds));
-                    aes.invSubBytes(bytematrix);
-                    aes.invShiftRows(bytematrix);
-                    for (int i = numRounds - 1; i > 0; i--) {
-                        aes.addRoundKey(bytematrix, aes.subKey(keymatrix, i));
-                        aes.invMixColumns(bytematrix);
-                        aes.invSubBytes(bytematrix); //implements the Sub-Bytes subroutine.
-                        aes.invShiftRows(bytematrix); //implements Shift-Rows subroutine.
-                    }
-                    aes.addRoundKey(bytematrix, aes.subKey(keymatrix, 0));
-                    out.write(MatrixToString(bytematrix) + '\n');
-                    line = input.readLine();
-                }
-                double time2 = (double) System.currentTimeMillis();
-                input.close();
-                out.close();
-                double total = ((time2 - time1) / 1000.0);
-                File f = new File(ftw + ".dec");
-                System.out.println("Took " + total + " seconds.");
-                System.out.println("Decryption throughput: " + ((double) (f.length() / 1000000.0) / total) + " MB/s");
-            } else {
-                System.err.println("Usage for Encryption: java AES e keyFile inputFile");
-                System.err.println("Usage for Decryption: java AES d keyFile encryptedinputFile");
+            keyreader = new BufferedReader(new FileReader(args[keyFileIndex]));
+            key = keyreader.readLine();
+            if(key.length() *4 != keysizecheck)
+            {
+                throw new Exception("Error: Attemping to use a " + key.length() * 4 + "-bit key with AES-"+keysizecheck);
             }
-        } catch (Exception e) {
+            
+            input = new BufferedReader(new FileReader(args[keyFileIndex+1]));
+            if(mode == Mode.CBC)
+            {
+                iv = keyreader.readLine();
+                assert iv.length() == 32;
+            }
+            ftw += args[keyFileIndex+1];
+        }
+        catch (Exception e) 
+        {
             System.err.println(e.getMessage() + '\n');
         }
+        
+        AES aes = new AES();
+        if (args[0].equalsIgnoreCase("e")) 
+        {
+            double time1 = (double) System.currentTimeMillis();
+            out = new FileWriter(ftw + ".enc");
+            int numRounds = 10 + (((key.length() * 4 - 128) / 32));
+            String line = input.readLine();
+            int[][] state, initvector = new int[4][4];
+            int[][] keymatrix = aes.keySchedule(key);
+            if(mode == Mode.CBC)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++) {
+                        initvector[j][i] = Integer.parseInt(line.substring((8 * i) + (2 * j), (8 * i) + (2 * j + 2)), 16);
+                    }
+                }                
+            }
+            while (line != null) {
+                if (aes.validline(line)) 
+                {
+                    if (line.length() < 32) {
+                        line = String.format("%" + 32 + "s", line).replace(' ', '0');
+                    }
+                    state = new int[4][4];
+                    for (int i = 0; i < 4; i++) //Parses line into a matrix
+                    {
+                        for (int j = 0; j < 4; j++) {
+                            state[j][i] = Integer.parseInt(line.substring((8 * i) + (2 * j), (8 * i) + (2 * j + 2)), 16);
+                        }
+                    }
+                    if(mode == Mode.CBC)
+                    {
+                        aes.addRoundKey(state, initvector);   
+                    }
+                    aes.addRoundKey(state, aes.subKey(keymatrix, 0)); //Starts the addRoundKey with the first part of Key Expansion
+                    for (int i = 1; i < numRounds; i++) {
+                        aes.subBytes(state); //implements the Sub-Bytes subroutine.
+                        aes.shiftRows(state); //implements Shift-Rows subroutine.
+                        aes.mixColumns(state);
+                        aes.addRoundKey(state, aes.subKey(keymatrix, i));
+                    }
+                    aes.subBytes(state); //implements the Sub-Bytes subroutine.
+                    aes.shiftRows(state); //implements Shift-Rows subroutine.
+                    aes.addRoundKey(state, aes.subKey(keymatrix, numRounds));
+                    if(mode == Mode.CBC)
+                    {
+                        initvector = state;
+                    }
+                    out.write(MatrixToString(state) + '\n');
+                    line = input.readLine();
+                } 
+                else 
+                {
+                    line = input.readLine();
+                }
+            }
+            double time2 = (double) System.currentTimeMillis();
+            input.close();
+            out.close();
+            File f = new File(ftw + ".enc");
+            double total = ((time2 - time1) / 1000.0);
+            System.out.println("Encryption throughput: " + ((double) (f.length() / 1000000.0) / total) + " MB/s");
+        } else if (args[0].equalsIgnoreCase("d")) {
+            double time1 = (double) System.currentTimeMillis();
+            out = new FileWriter(ftw + ".dec");
+            int numRounds = 10 + (((key.length() * 4 - 128) / 32));
+            String line = input.readLine();
+            int[][] bytematrix;
+            int[][] keymatrix = aes.keySchedule(key);
+
+            while (line != null) {
+                bytematrix = new int[4][4];
+                for (int i = 0; i < bytematrix.length; i++) //Parses line into a matrix
+                {
+                    for (int j = 0; j < bytematrix[0].length; j++) {
+                        bytematrix[j][i] = Integer.parseInt(line.substring((8 * i) + (2 * j), (8 * i) + (2 * j + 2)), 16);
+                    }
+                }
+                aes.addRoundKey(bytematrix, aes.subKey(keymatrix, numRounds));
+                aes.invSubBytes(bytematrix);
+                aes.invShiftRows(bytematrix);
+                for (int i = numRounds - 1; i > 0; i--) {
+                    aes.addRoundKey(bytematrix, aes.subKey(keymatrix, i));
+                    aes.invMixColumns(bytematrix);
+                    aes.invSubBytes(bytematrix); //implements the Sub-Bytes subroutine.
+                    aes.invShiftRows(bytematrix); //implements Shift-Rows subroutine.
+                }
+                aes.addRoundKey(bytematrix, aes.subKey(keymatrix, 0));
+                out.write(MatrixToString(bytematrix) + '\n');
+                line = input.readLine();
+            }
+            double time2 = (double) System.currentTimeMillis();
+            input.close();
+            out.close();
+            double total = ((time2 - time1) / 1000.0);
+            File f = new File(ftw + ".dec");
+            System.out.println("Took " + total + " seconds.");
+            System.out.println("Decryption throughput: " + ((double) (f.length() / 1000000.0) / total) + " MB/s");
+        } else {
+            System.err.println("Usage for Encryption: java AES e keyFile inputFile");
+            System.err.println("Usage for Decryption: java AES d keyFile encryptedinputFile");
+        } 
     }
 
     /**
@@ -495,7 +548,7 @@ public class AES {
     /**
      * In the AddRoundKey step, the subkey is combined with the state. For each round, a chunk of the key scheduled is pulled; each subkey is the same size as the state. Each element in the byte matrix is XOR'd with each element in the chunk of the expanded key.
      * 
-     * @param bytematrix reference of the matrix in which addRoundKey will be computed upon.
+     * @param state reference of the matrix in which addRoundKey will be computed upon.
      * @param keymatrix chunk of the expanded key
      */
 
